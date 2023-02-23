00b4191c9b40172faf34605efa53055c24a050d6
An10113 <antranthanh1011@gmail.com>
2023-02-23 10:02:14 +0700

    8

--- src/component/ul/Book.jsx
+++ src/component/ul/Book.jsx
@@ -1,26 +1,57 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Price from "./Price";
 import Ratings from "./Ratings";
 
 
 const Book = ({book}) => {
-   return ( <div className="book">
+  const [img,setImg]= useState()
+  const mountedRef = useRef(false);
+  useEffect(()=>{
+    const image = new Image()
+    image.src = book.url
+    image.onload =() => {
+      setTimeout(() => {
+        if(mountedRef.current){
+          setImg(image)
+        }
+      },500)
+    }
+    return () => {
+      // When the component unmounts 
+        mountedRef.current = true;
+    };
+  }, [book.url])
+  return ( 
+    <div className="book">
+      { img ? 
+        (<>
         <Link to={`/books/${book.id}`}>
-            <figure className="book__img--wrapper">
-                <img src={book.url} alt="" className="book__img"/>
-            </figure>
+        <figure className="book__img--wrapper">
+        <img src={book.url} alt="" className="book__img"/>
+        </figure>
         <div className="book__title">
-            <Link to={`/books/${book.id}`} className="book__link">
-                {book.title}
-            </Link>
+        <Link to={`/books/${book.id}`} className="book__link">
+        {book.title}
+        </Link>
         </div>
         <Ratings rating={book.rating} />
         <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
-        </Link>
-    </div>
-   )
+      </Link>
+      </>)
+      : 
+        (
+        <>
+        <div className="book__img--skeleton"></div>
+        <div className="skeleton book__title--skeleton"></div>
+        <div className="skeleton book__rating--skeleton"></div>
+        <div className="skeleton book__price--skeleton"></div>
+        </>
+        )
+    }
+</div>
+)
 }
 
 export default Book
